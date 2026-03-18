010e5c6d17fecc4db185b2c93865e206c73d8326
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2026-03-18 14:16:47 +0000

    Deleted a couple of files those files are on another branch that is designed to clean up the bank dashboard, so there is no need to pollute this branch with something that is not yet completed.

--- static/js/dashboard/credit-card-overview/source-card/card-utils.js
+++
@@ -1,8 +0,0 @@
-import { getCardDetailsFromElement } from "../card-details-extractor.js";
-import { transferFormSelectOption, cardSelectionPanelState, selectedCardStore } from "../card-state-store.js";
-import { formatCurrency, deselectAllElements, toggleElement } from "../../../utils.js";
-import { AlertUtils } from "../../../alerts.js";
-import { cardImplementer } from "../../../card/cardBuilder.js";
-import { warnError } from "../../../logger.js";
-
-
